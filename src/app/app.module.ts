1ab3cb4a2efe0822ac75d5f230ae3b300836b437
Matthew Cataldi <mcataldi@ironarchtech.com>
2020-09-01 08:28:53 -0500

    Implemented the Success Component manually without "ng generate component success"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 // TypeScript Components need to be registered with the App Module so that way it know what to load
 // This needs to be explicit
@@ -12,7 +13,8 @@ import { WarningComponent } from './warning/warning.component';
     AppComponent,
     ServerComponent,
     ServersComponent,
-    WarningComponent
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule
