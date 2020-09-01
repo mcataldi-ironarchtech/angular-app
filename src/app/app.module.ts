7922bce833b978374c1c5ce3ecaf724426763957
Matthew Cataldi <mcataldi@ironarchtech.com>
2020-09-01 08:11:48 -0500

    Committed warning component and tested.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
 
 // TypeScript Components need to be registered with the App Module so that way it know what to load
 // This needs to be explicit
@@ -10,7 +11,8 @@ import { ServersComponent } from './servers/servers.component';
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule
